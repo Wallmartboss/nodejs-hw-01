06a2777de76a9c03ec6f4b52552c267cf0848c71
Yuri <wall.mart.boss@proton.me>
2024-08-24 01:26:58 +0300

    Update addOneContact.js

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -2,7 +2,7 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {
+const addOneContact = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
