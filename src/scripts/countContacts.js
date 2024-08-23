f81dcdbc9c168adaecf0059e40a92239e86ad590
Yuri <wall.mart.boss@proton.me>
2024-08-24 01:24:27 +0300

    final

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    console.log(contacts.length);
+  } catch (error) {
+    console.error('Failed to read contacts from the file:', error);
+  }
+};
 
-console.log(await countContacts());
+countContacts();
+export default countContacts;
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const getAllContacts = async () => {};
+const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    console.log(contacts);
+  } catch (error) {
+    console.error('Failed to read contacts from the file:', error);
+  }
+};
 
-console.log(await getAllContacts());
+getAllContacts();
+
+export default getAllContacts;
